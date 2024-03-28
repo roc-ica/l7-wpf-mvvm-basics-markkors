6f6cc59346f1637a59f1d319deeb0b3239b6c0d5
markk <m.kors@rocmn.nl>
2024-03-28 09:44:59 +0100

    Add project files.

---
+++ Model/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace WpfApp7.Model
+{
+    internal class BankAccount 
+    {
+       
+
+        // fields
+        private decimal _saldo;
+
+        // constructor
+        public BankAccount(decimal saldo)
+        {
+            _saldo = saldo;
+          
+        }
+
+        // properties
+        public decimal Balance { 
+            get {
+                return _saldo;
+            } 
+           /* set {
+                _saldo = value;
+            } */
+        }
+
+        
+
+
+        // methods
+        public void Deposit(decimal amount)
+        {
+            _saldo += amount;
+
+        }
+
+        public Boolean Withdraw(decimal amount)
+        {
+            // not allowed to withdraw more than the saldo
+            if (amount > _saldo)
+            {
+                return false;
+            }
+            _saldo -= amount;
+
+            return true;
+        }
+
+       
+        
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp7.Model;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BankAccount ba1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ba1 = new BankAccount(100);
+            // update the display
+            updateDisplay();
+        }
+
+        #region methods
+        private void updateDisplay()
+        {
+            txtBalance.Text = ba1.Balance.ToString("C");
+        }
+        #endregion
+
+
+        #region event handlers
+
+
+        private void doDeposit(object sender, RoutedEventArgs e)
+        {
+
+
+            decimal amount = decimal.Parse(txtAmount.Text);
+            ba1.Deposit(amount);
+            updateDisplay();
+        }
+
+        private void doWithdraw(object sender, RoutedEventArgs e)
+        {
+            decimal amount = decimal.Parse(txtAmount.Text);
+            ba1.Withdraw(amount);
+            updateDisplay();
+        }
+
+        #endregion
+
+
+    }
+}
