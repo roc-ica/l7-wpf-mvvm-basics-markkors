6fee960905376b42d62f4da0850621f2aa173717
Mark Kors <mark.kors@gmail.com>
2024-03-29 09:28:32 +0100

    Euro Converter toegevoegd

---
+++ Model/DecimalToEuroConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp7.Model
+{
+    internal class DecimalToEuroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Convert the decimal value to a string with euro sign
+            var euroCulture = CultureInfo.GetCultureInfo("nl-NL");
+            // Convert the decimal value to a string with euro sign
+            return ((decimal)value).ToString("C", euroCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
